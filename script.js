4f8637225132389f49c447c902f0220eed6ecee8
Caleb Parris <calebparris@live.com>
2020-08-03 14:45:10 -0500

    First Question Displaying

--- script.js
+++ script.js
@@ -1,68 +1,35 @@
+var questions = ["Commonly used data types DO NOT include:", "The condition in an if/else statement is enclosed with _____.", "Arrays in Javascript cna be used to store _____.", "String values must be enclosed within _____ when beign assigned to variables.", "A very useful tool used during development and debugging for printing content to the debugger is:"];
+var firstChoices = ["strings", "booleans", "alerts", "numbers"];
+var secondChoices = ["quotes", "curly brackets", "parenthesis", "square brackets"];
+var thirdChoices = ["numbers and strings", "other arrays", "booleans", "all of the above"];
+var fourthChoices = ["commas", "curly brackets", "quotes", "parenthesis"];
+var fifthChoices = ["Javascript", "terminal / bash", "for loops", "console.log"];
+
 var startBtn = document.querySelector("#start-btn");
-var nextBtn = document.querySelector("#next-btn");
-var scoreBtn = document.querySelector("#highScore-btn");
-var timerElement = document.querySelector("#timer");
-var titleElement = document.querySelector("#title");
-var extraTextElement = document.querySelector("#extra-text");
-var questionContainer = document.querySelector("#question-container");
 var questionElement = document.querySelector("#question");
-var formElement = document.querySelector("#form");
-var backBtn = document.querySelector("#back-btn");
-var clearBtn = document.querySelector("#clear-btn");
-var leaderboardElement = document.querySelector("#leaderboard");
-var answerButtonsElement = document.querySelector("#answer-buttons");
-var submitBtn = document.querySelector("#submit-btn");
-var choices = document.querySelectorAll(".answer-text");
 
-var score = 0;
-var currentQuestion;
+var questionIndex = 0;
 
-
-var questionPool = [
-    {
-        question: "Commonly used data types DO NOT include:",
-        answers: ["strings", "booleans", "alerts", "numbers"],
-        correct: 2
-    },
-    {
-        question: "The condition in an if/else statement is enclosed with _____.",
-        answers: ["parenthesis", "curly brackets", "quotes", "square brackets"],
-        correct: 0
-    },
-    {
-        question: "Arrays in Javascript cna be used to store _____.",
-        answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        correct: 3
-    },
-    {
-        question: "String values must be enclosed within _____ when beign assigned to variables.",
-        answers: ["commas", "quotes", "curly brackets", "parenthesis"],
-        correct: 1
-    },
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: ["Javascript", "terminal / bash", "for loops", "console.log"],
-        correct: 3
-    }
-]
+startBtn.addEventListener("click", function(){
+    startQuiz();
+})
 
 function startQuiz(){
-
+    document.querySelector("#start-btn").setAttribute("class", "hidden");
+    document.querySelector("#highscore-btn").setAttribute("class", "hidden");
+    document.querySelector("#extra-text").setAttribute("class", "hidden");
+    document.querySelector("#question-container").removeAttribute("class", "hidden");
+    displayFirstQuestion();
 }
 
-function nextQuestion(){
+function displayFirstQuestion(){
+    questionElement.textContent = questions[questionIndex];
+    for (var i = 0; i < firstChoices.length; i++){
+        var answerChoices = document.createElement("button");
+        answerChoices.setAttribute("class", "btn");
+        answerChoices.setAttribute("data-index", i)
+        answerChoices.textContent = firstChoices[i];
+        document.body.children[1].children[2].children[1].appendChild(answerChoices);
+    }
 
 }
-
-function displayQuestion(){
-
-}
-
-function setTimer(){
-
-}
-
-function checkAnswer(){
-
-}
-
