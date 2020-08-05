698c4ba2365a8f50c4b0c349b2258ad74dfc5a22
Caleb Parris <calebparris@live.com>
2020-08-04 22:52:28 -0500

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Code-Quiz
+
+## Objective
+This homework was to create a quiz that would dynamically change questions and answers. Another objective was to have a timer that displayed throughout the quiz and lost time for wrong answers. The last objective was to save user score data and save it in the local storage.
+
+## Challenges & Solutions
+The first challenge I came across was getting the questions to change dynamically. I had tried to use and array of objects that held all the questions and answers, but I was unable to get the quiz to function this way. I solved this by creating individual arrays for the answer choices and questions. The next challenge I came across was to get the user data stored in local storage and displayed on the page. The solution I had for this was to store the information in an object and create a list element for each name entry so that it would display on the page.
+
+## Links and Pictures
+Here is a link to the finished quiz: https://calebparris.github.io/Code-Quiz/
+
+Here is a screenshot of the starting page:
+
+![Quiz Starting Page](Quiz.png)
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//This section delcares the arrays for the questions and answers for each question
 var questions = ["Commonly used data types DO NOT include:", "The condition in an if/else statement is enclosed with _____.", "Arrays in Javascript can be used to store _____.", "String values must be enclosed within _____ when beign assigned to variables.", "A very useful tool used during development and debugging for printing content to the debugger is:"];
 var firstChoices = ["strings", "booleans", "alerts", "numbers"];
 var secondChoices = ["quotes", "curly brackets", "parenthesis", "square brackets"];
@@ -5,6 +6,7 @@ var thirdChoices = ["numbers and strings", "other arrays", "booleans", "all of t
 var fourthChoices = ["quotes", "curly brackets", "commas", "parenthesis"];
 var fifthChoices = ["Javascript", "console.log", "for loops", "terminal / bash"];
 
+//This section declares variables that point to the HTML elements
 var startBtn = document.querySelector("#start-btn");
 var questionElement = document.querySelector("#question");
 var answerBtnElement = document.querySelector("#answer-buttons")
@@ -20,17 +22,58 @@ var backBtn = document.querySelector("#back-btn");
 var clearBtn = document.querySelector("#clear-btn");
 var formElement = document.querySelector("#form");
 
+//This section delcares variables that will be changes throughout the rest of the code
 var questionIndex = 0;
 var score = 0;
 var secondsLeft = 50;
 var timeInterval;
 
-
+//These event listeners determine the functionality of the button clicks
 startBtn.addEventListener("click", function(){
     startQuiz();
     setTimer();
 });
 
+submitBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    var leaderboardInfo = {
+        name: nameInput.value,
+        points: score
+    };
+    if (leaderboardInfo.name === ""){
+        return false;
+    } else {
+        localStorage.setItem("leaderboardInfo", JSON.stringify(leaderboardInfo));
+
+        var newestScore = JSON.parse(localStorage.getItem("leaderboardInfo"));
+        var scoreList = document.createElement("li");
+        scoreList.textContent = leaderboardInfo.name + " - " + leaderboardInfo.points;
+        document.body.children[1].children[5].children[0].appendChild(scoreList);
+    }
+    leaderboardPage();
+});
+
+highscoreBtn.addEventListener("click", function(){
+leaderboardPage();
+});
+
+backBtn.addEventListener("click", function(){
+    document.querySelector("#score-container").classList.add("hidden");
+    document.querySelector("#highscore-btn").classList.remove("hidden");
+    document.querySelector("#start-btn").classList.remove("hidden");
+    document.querySelector("#extra-text").classList.remove("hidden");
+    titleElement.textContent = "Coding Quiz Challenge";
+    extraTextElement.textContent = "Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will reduce the timer by 10 seconds.";
+});
+
+clearBtn.addEventListener("click", function(){
+    localStorage.clear();
+    while (leaderboardElement.firstChild){
+        leaderboardElement.removeChild(leaderboardElement.firstChild);
+    }
+});
+
+//These functions determine the page layout
 function startQuiz(){
     document.querySelector("#start-btn").classList.add("hidden");
     document.querySelector("#highscore-btn").classList.add("hidden");
@@ -62,12 +105,6 @@ function displayFirstQuestion(){
             resetAnswers();
             displaySecondQuestion();
         });
-    }
-}
-
-function resetAnswers(){
-    while (answerBtnElement.firstChild){
-        answerBtnElement.removeChild(answerBtnElement.firstChild);
     }
 }
 
@@ -170,6 +207,26 @@ function displayFifthQuestion(){
     }
 }
 
+function recordNameScore(){
+    document.querySelector("#question-container").classList.add("hidden");
+    document.querySelector("#extra-text").classList.remove("hidden");
+    document.querySelector("#form").classList.remove("hidden");
+    document.querySelector("#timer").classList.add("hidden");
+    extraTextElement.textContent = "Your final score is: " + score;
+    clearInterval(timeInterval);
+    formElement.reset();
+}
+
+function leaderboardPage(){
+    document.querySelector("#score-container").classList.remove("hidden");
+    document.querySelector("#highscore-btn").classList.add("hidden");
+    document.querySelector("#start-btn").classList.add("hidden");
+    document.querySelector("#extra-text").classList.add("hidden");
+    document.querySelector("#form").classList.add("hidden");
+    titleElement.textContent = "Highscore Leaderboard";
+}
+
+//This function creates the timer
 function setTimer(){
     timeInterval = setInterval(function(){
         secondsLeft--;
@@ -182,61 +239,9 @@ function setTimer(){
     }, 1000);
 }
 
-function recordNameScore(){
-    document.querySelector("#question-container").classList.add("hidden");
-    document.querySelector("#extra-text").classList.remove("hidden");
-    document.querySelector("#form").classList.remove("hidden");
-    document.querySelector("#timer").classList.add("hidden");
-    extraTextElement.textContent = "Your final score is: " + score;
-    clearInterval(timeInterval);
-    formElement.reset();
-}
-
-submitBtn.addEventListener("click", function(event){
-        event.preventDefault();
-        var leaderboardInfo = {
-            name: nameInput.value,
-            points: score
-        };
-        if (leaderboardInfo.name === ""){
-            return false;
-        } else {
-            localStorage.setItem("leaderboardInfo", JSON.stringify(leaderboardInfo));
-
-            var newestScore = JSON.parse(localStorage.getItem("leaderboardInfo"));
-            var scoreList = document.createElement("li");
-            scoreList.textContent = leaderboardInfo.name + " - " + leaderboardInfo.points;
-            document.body.children[1].children[5].children[0].appendChild(scoreList);
-        }
-        leaderboardPage();
-    });
-
-highscoreBtn.addEventListener("click", function(){
-    leaderboardPage();
-});
-
-function leaderboardPage(){
-    document.querySelector("#score-container").classList.remove("hidden");
-    document.querySelector("#highscore-btn").classList.add("hidden");
-    document.querySelector("#start-btn").classList.add("hidden");
-    document.querySelector("#extra-text").classList.add("hidden");
-    document.querySelector("#form").classList.add("hidden");
-    titleElement.textContent = "Highscore Leaderboard";
-}
-
-backBtn.addEventListener("click", function(){
-    document.querySelector("#score-container").classList.add("hidden");
-    document.querySelector("#highscore-btn").classList.remove("hidden");
-    document.querySelector("#start-btn").classList.remove("hidden");
-    document.querySelector("#extra-text").classList.remove("hidden");
-    titleElement.textContent = "Coding Quiz Challenge";
-    extraTextElement.textContent = "Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will reduce the timer by 10 seconds.";
-});
-
-clearBtn.addEventListener("click", function(){
-    localStorage.clear();
-    while (leaderboardElement.firstChild){
-        leaderboardElement.removeChild(leaderboardElement.firstChild);
+//This function resets the answers between questions
+function resetAnswers(){
+    while (answerBtnElement.firstChild){
+        answerBtnElement.removeChild(answerBtnElement.firstChild);
     }
-});
-
+}
